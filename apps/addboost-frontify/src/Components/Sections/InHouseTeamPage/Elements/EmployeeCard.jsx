f444b7e024ba57f4b942ca526b2286a89e368d4a
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-07-09 12:16:15 +0530

    Update EmployeeCard.jsx

--- apps/addboost-frontify/src/Components/Sections/InHouseTeamPage/Elements/EmployeeCard.jsx
+++ apps/addboost-frontify/src/Components/Sections/InHouseTeamPage/Elements/EmployeeCard.jsx
@@ -2,7 +2,7 @@ import Cornerchip from "../../../../assets/Cornerchip.svg";
 
 const EmployeeCard = ({ employee }) => {
   return (
-    <div className="bg-white hover:bg-primary group transition-colors duration-300 p-4 md:p-6 lg:p-8 relative overflow-hidden h-full ">
+    <div className="bg-white  transition-colors duration-300 p-4 md:p-6 lg:p-8 relative overflow-hidden h-full ">
       {/* Corner Chip */}
       <div className="absolute top-0 left-0">
         <img src={Cornerchip} alt="Global" className="w-6 h-6" />
@@ -16,15 +16,15 @@ const EmployeeCard = ({ employee }) => {
           className="w-24 h-24 mx-auto rounded-full object-cover border-4 border-white transition-colors duration-300"
         />
 
-        <h3 className="mt-4 font-semibold text-sm sm:text-lg md:text-xl text-black font-arya group-hover:text-white transition-colors duration-300">
+        <h3 className="mt-4 font-semibold text-sm sm:text-lg md:text-xl text-black font-arya ">
           {employee.title}
         </h3>
 
-        <p className="text-primary font-medium text-xs sm:text-base md:text-lg font-inter group-hover:text-white transition-colors duration-300">
+        <p className="text-primary font-medium text-xs sm:text-base md:text-lg font-inter ">
           {employee.designation}
         </p>
 
-        <p className="text-gray-600 text-xs sm:text-sm md:text-base font-inter mb-5 group-hover:text-white transition-colors duration-300">
+        <p className="text-gray-600 text-xs sm:text-sm md:text-base font-inter mb-5 ">
           {employee.country}
         </p>
       </div>
