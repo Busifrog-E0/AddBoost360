eaf0ca8b5e8100d36894bfae300b50568563fde1
Haris Wilson <harismoylan@gmail.com>
2025-07-22 19:21:18 +0530

    debug added

--- apps/server/db.config.js
+++ apps/server/db.config.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 import ENV from "./Env.js";
 
 // const uri = `mongodb://${ENV.DB_USER}:${ENV.DB_PASSWORD}@localhost:27017/${ENV.DATABASE}?maxPoolSize=2-&w=majority`;
-const uri = ENV.ModeOfDevelopment === "Production" ? ENV.MongoDB_Host : `mongodb://0.0.0.0:27017`;
+const uri = ENV.ModeOfDevelopment === "Production" ? ENV.MongoDB_Host : ENV.MongoDB_Debug_Host;
 
 const client = new MongoClient(uri);
 
