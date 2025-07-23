d0322d0f38609104ea96172ad59ae16c61fb4de8
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-07-23 16:34:59 +0530

    padding

--- apps/addboost-frontify/src/Components/Sections/ServicePage/ServiceHeroSection.jsx
+++ apps/addboost-frontify/src/Components/Sections/ServicePage/ServiceHeroSection.jsx
@@ -16,7 +16,15 @@ const ServiceHeroSection = () => {
         ></div>
 
         {/* Text Content */}
-        <div className="relative z-10 p-6 sm:p-10 md:p-20 lg:p-20 text-white flex flex-col items-center text-center justify-center min-h-screen lg:items-start lg:text-left">
+        <div className="relative z-10 
+        pr-6 md:pr-14 xl:pr-28
+         pl-6 md:pl-14 2xl:pl-60 3xl:pl-80 
+  4xl:pl-120 5xl:pl-160 6xl:pl-180
+  7xl:pl-220 8xl:pl-240 9xl:pl-260
+  10xl:pl-280 11xl:pl-300 12xl:pl-320
+  13xl:pl-340 14xl:pl-360 15xl:pl-400
+   py-6 sm:py-10 lg:py-20 
+        text-white flex flex-col items-center text-center justify-center min-h-screen lg:items-start lg:text-left">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-4xl xl:text-6xl font-anton uppercase leading-snug">
             Smart Solutions to Launch, Scale, and Empower Your Business
           </h1>
