1087de2cab7d2d5757c48a7361038225a8ae5bb6
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-07-23 18:18:12 +0530

    Update Sidebar.jsx

--- apps/addboost-frontify/src/Components/Sidebar.jsx
+++ apps/addboost-frontify/src/Components/Sidebar.jsx
@@ -15,7 +15,7 @@ const Sidebar = ({ menuOpen, setMenuOpen, navItems }) => {
   return (
     <div
       className={`
-    fixed inset-0 bg-black z-50 transform transition-transform duration-300 ease-in-out
+    fixed inset-0 bg-black z-[1000] transform transition-transform duration-300 ease-in-out
     ${menuOpen ? "translate-x-0" : "translate-x-full"}
     overflow-y-auto h-screen
   `}
