f6c6fb0b5268be5e9bb75f67fe2830a3ffa5e6bf
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-07-28 12:34:36 +0530

    Update Sidebar.jsx

--- apps/addboost-sparkpanel/src/components/ui/Sidebar.jsx
+++ apps/addboost-sparkpanel/src/components/ui/Sidebar.jsx
@@ -8,7 +8,7 @@ import {
   Quote,
   Home,
   LogOut,
-  Image,
+  Handshake,
 } from "lucide-react";
 import { useNavigate } from "react-router";
 import { useAuth } from "../../features/auth/context/AuthProvider";
@@ -29,7 +29,7 @@ const Sidebar = ({
     { id: "team", label: "Team", icon: Users },
     { id: "companies", label: "Companies", icon: Building2 },
     { id: "form", label: "Leads", icon: UserSearch },
-    { id: "providers", label: "Service Providers", icon: Image },
+    { id: "providers", label: "Service Providers", icon: Handshake },
     { id: "review", label: "Success Stories", icon: Quote },
   ];
 
@@ -70,14 +70,14 @@ const Sidebar = ({
                     onCloseMobile && onCloseMobile(); // 👈 Close on mobile
                   }}
                   className={`w-full flex items-center space-x-3 px-3 py-2.5 rounded-lg transition-all duration-200 group ${isActive
-                      ? "bg-blue-600 text-white shadow-lg"
-                      : "hover:bg-slate-700 text-slate-300 hover:text-white"
+                    ? "bg-blue-600 text-white shadow-lg"
+                    : "hover:bg-slate-700 text-slate-300 hover:text-white"
                     }`}
                 >
                   <Icon
                     className={`w-5 h-5 ${isActive
-                        ? "text-white"
-                        : "text-slate-400 group-hover:text-white"
+                      ? "text-white"
+                      : "text-slate-400 group-hover:text-white"
                       }`}
                   />
                   {!isCollapsed && (
