af6b2f44a5361ba979a6759b257a4efb6f826d2e
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-07-31 15:18:58 +0530

    Update ContactPage.jsx

--- apps/addboost-frontify/src/Components/Pages/ContactPage.jsx
+++ apps/addboost-frontify/src/Components/Pages/ContactPage.jsx
@@ -7,7 +7,19 @@ import Button from "../Button";
 const ContactPage = () => {
   return (
     <div>
-      <div className="bg-PrimaryDarkBlue px-6 md:px-14 2xl:px-60 3xl:px-80 py-14 pt-32 md:py-20 md:pt-40 lg:py-24 lg:pt-44">
+      <div className="bg-PrimaryDarkBlue
+      
+
+
+       px-6 md:px-10 2xl:px-24 3xl:px-32
+4xl:px-60 5xl:px-80 6xl:px-180
+  7xl:px-220 8xl:px-240 9xl:px-260
+  10xl:px-280 11xl:px-300 12xl:px-320
+  13xl:px-340 14xl:px-360 15xl:px-400
+
+ 
+
+      py-10 pt-32 md:py-14 md:pt-40 lg:py-20 lg:pt-44">
         {/* Heading Section */}
         <p className="text-white font-inter text-sm 2xl:text-lg">
           Let's Unlock Your Brand's Full Potential — Together.
