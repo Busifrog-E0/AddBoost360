086175a6bdb2e7f8ea01b4d2c0b3c34addd82ded
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-07-31 21:00:14 +0530

    Update PortfolioCard.jsx

--- apps/addboost-frontify/src/Components/Sections/PortfolioPage/PortfolioCard.jsx
+++ apps/addboost-frontify/src/Components/Sections/PortfolioPage/PortfolioCard.jsx
@@ -146,8 +146,17 @@ const PortfolioCard = ({ project, reverse = false, bgColor = "bg-white" }) => {
   return (
     <div className={`${bgColor}`}>
       <div
-        className="grid lg:grid-cols-2 gap-10 lg:gap-20 items-start px-6 md:px-14 2xl:px-60 3xl:px-80 
-        py-14 md:py-20 lg:py-24"
+        className="grid lg:grid-cols-2 gap-10 lg:gap-20 items-start px-6 md:px-10 2xl:px-24 3xl:px-32
+4xl:px-60 5xl:px-80 6xl:px-180
+  7xl:px-220 8xl:px-240 9xl:px-260
+  10xl:px-280 11xl:px-300 12xl:px-320
+  13xl:px-340 14xl:px-360 15xl:px-400
+
+py-10 md:py-14 lg:py-20
+4xl:py-24 5xl:py-28 6xl:py-32 7xl:py-36
+8xl:py-40 9xl:py-44 10xl:py-48
+11xl:py-52 12xl:py-56 13xl:py-60
+14xl:py-64 15xl:py-72"
       >
         {reverse ? (
           <>
