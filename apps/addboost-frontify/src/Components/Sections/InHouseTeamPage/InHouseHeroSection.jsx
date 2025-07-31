07d2f2d71233cbd5d64745bbb3899251cd90ddae
swapnakt <swapnakt103@gmail.com>
2025-07-31 16:19:35 +0530

    TalentPool

--- apps/addboost-frontify/src/Components/Sections/InHouseTeamPage/InHouseHeroSection.jsx
+++ apps/addboost-frontify/src/Components/Sections/InHouseTeamPage/InHouseHeroSection.jsx
@@ -31,17 +31,12 @@ const imageVariant = {
 
 const InHouseHeroSection = () => {
   return (
-    <div className="bg-PrimaryDarkBlue grid grid-cols-1 lg:grid-cols-2 items-center lg:min-h-0 min-h-[900px] lg:mt-0 gap-10 lg:gap-20 
-    px-6 md:px-10 2xl:px-24 3xl:px-32
- 
-4xl:py-24 5xl:py-28 6xl:py-32 7xl:py-36
-8xl:py-40 9xl:py-44 10xl:py-48
-11xl:py-52 12xl:py-56 13xl:py-60
-14xl:py-64 15xl:py-72
-
-    py-10 pt-32 md:py-14 md:pt-40 lg:py-20 lg:pt-44 
+    <div
+      className="bg-PrimaryDarkBlue grid grid-cols-1 lg:grid-cols-2 items-center lg:min-h-0 min-h-[900px] lg:mt-0 gap-10 lg:gap-20 
+   
     
-    relative">
+    relative"
+    >
       {/* Background Image for Mobile */}
       <div
         className="absolute inset-0 lg:hidden bg-cover bg-center opacity-20 z-0"
@@ -54,7 +49,14 @@ const InHouseHeroSection = () => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true }}
-        className="text-white text-center lg:text-left flex flex-col items-center lg:items-start z-10"
+        className="text-white text-center lg:text-left flex flex-col items-center lg:items-start z-10  px-6 md:px-10 2xl:px-24 3xl:px-32
+ 
+4xl:py-24 5xl:py-28 6xl:py-32 7xl:py-36
+8xl:py-40 9xl:py-44 10xl:py-48
+11xl:py-52 12xl:py-56 13xl:py-60
+14xl:py-64 15xl:py-72
+
+    py-10 pt-32 md:py-14 md:pt-40 lg:py-20 lg:pt-44 "
       >
         <motion.p
           variants={textVariant}
@@ -84,12 +86,12 @@ const InHouseHeroSection = () => {
         initial="hidden"
         whileInView="show"
         viewport={{ once: true }}
-        className="hidden lg:block h-[560px] z-10"
+        className="hidden lg:block  z-10"
       >
         <img
           src={InHouseHeroImage}
           alt="Talent Pool"
-          className="h-full w-full object-cover rounded-md"
+          className=" w-full object-cover "
         />
       </motion.div>
     </div>
