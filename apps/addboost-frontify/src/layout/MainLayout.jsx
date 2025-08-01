cb0fbc51cef52562726a25c9e310a6e1a654519d
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-08-01 23:04:57 +0530

    changes

--- apps/addboost-frontify/src/layout/MainLayout.jsx
+++ apps/addboost-frontify/src/layout/MainLayout.jsx
@@ -1,36 +1,8 @@
-// import { Outlet, useLocation } from "react-router";
-// import { useEffect, useState, useLayoutEffect } from "react";
-// import Header from "../Components/Header";
-// import Footer from "../Components/Footer";
-
-// import { useRef } from "react";
-// import ScrollToTopButton from "../Components/ScrollToTopButton";
-
-// const MainLayout = () => {
-//   const location = useLocation();
-
-//   useLayoutEffect(() => {
-//     window.scrollTo({ top: 0, behavior: "smooth" });
-//   }, [location]);
-
-//   return (
-//     <div>
-//       <Header />
-//       <Outlet />
-//       <Footer />
-
-//       <ScrollToTopButton />
-//     </div>
-//   );
-// };
-
-// export default MainLayout;
-import { lazy, Suspense, useLayoutEffect, } from "react";
 import { Outlet, useLocation } from "react-router";
-
-const Header = lazy(() => import("../Components/Header"));
-const Footer = lazy(() => import("../Components/Footer"));
-const ScrollToTopButton = lazy(() => import("../Components/ScrollToTopButton"));
+import { useLayoutEffect } from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import ScrollToTopButton from "../Components/ScrollToTopButton";
 
 const MainLayout = () => {
   const location = useLocation();
@@ -41,16 +13,10 @@ const MainLayout = () => {
 
   return (
     <div>
-      <Suspense fallback={<div />}>
-        <Header />
-      </Suspense>
-
+      <Header />
       <Outlet />
-
-      <Suspense fallback={<div />}>
-        <Footer />
-        <ScrollToTopButton />
-      </Suspense>
+      <Footer />
+      <ScrollToTopButton />
     </div>
   );
 };
