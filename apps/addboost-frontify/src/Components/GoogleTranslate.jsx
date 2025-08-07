ae1ba868546251f0c0768cfe8d6a39013f4ae2ed
swapnakt <swapnakt103@gmail.com>
2025-08-07 14:56:58 +0530

    language

--- apps/addboost-frontify/src/Components/GoogleTranslate.jsx
+++ apps/addboost-frontify/src/Components/GoogleTranslate.jsx
@@ -4,35 +4,108 @@ import GoogleTranslateLoader from "./GoogleTranslateLoader";
 
 import Ukflag from "../assets/Ukflag.webp";
 import Arabflag from "../assets/Arabflag.webp";
-
+import Germany from "../assets/Flags/Germany.jpg";
+import Spain from "../assets/Flags/Spain.jpg";
+import India from "../assets/Flags/India.png";
+import Japan from "../assets/Flags/Japan.png";
+import Korea from "../assets/Flags/Korea.png";
+import Indonesia from "../assets/Flags/Indonesia.png";
+import China from "../assets/Flags/China.png";
+import France from "../assets/Flags/France.png";
+import Italy from "../assets/Flags/Italy.png";
+import Poland from "../assets/Flags/Poland.png";
+import Philippines from "../assets/Flags/Philippines.png";
+import Vietnam from "../assets/Flags/Vietnam.png";
 export const languages = [
-    {
-        code: "en",
-        name: "English",
-        flag: Ukflag,
-    },
-    {
-        code: "ar",
-        name: "Arabic",
-        flag: Arabflag,
-    },
+  {
+    code: "en",
+    name: "English",
+    flag: Ukflag,
+  },
+  {
+    code: "ar",
+    name: "Arabic",
+    flag: Arabflag,
+  },
+  {
+    code: "de",
+    name: "German",
+    flag: Germany,
+  },
+  {
+    code: "ja",
+    name: "Japanese",
+    flag: Japan,
+  },
+  {
+    code: "hi",
+    name: "Hindi",
+    flag: India,
+  },
+  {
+    code: "ko",
+    name: "Korean",
+    flag: Korea,
+  },
+  {
+    code: "id",
+    name: "Bahasa Indonesia",
+    flag: Indonesia,
+  },
+
+  {
+    code: "fr",
+    name: "French",
+    flag: France,
+  },
+  {
+    code: "es",
+    name: "Spanish",
+    flag: Spain,
+  },
+  {
+    code: "it",
+    name: "Italian",
+    flag: Italy,
+  },
+  {
+    code: "pl",
+    name: "Polish",
+    flag: Poland,
+  },
+  {
+    code: "tl",
+    name: "Tagalog ",
+    flag: Philippines,
+  },
+  {
+    code: "vi",
+    name: "Vietnamese ",
+    flag: Vietnam,
+  },
+  {
+    code: "zh-CN",
+    name: "Chinese",
+    flag: China,
+  },
 ];
 
-const GoogleTranslate = ({ }) => {
-    const [selectedLang, setSelectedLang] = useState(localStorage.getItem("selectedLanguage") || "en");
-    const [isComboReady, setIsComboReady] = useState(false);
-    return (
-        <>
-            <GoogleTranslateLoader
-                onReady={setIsComboReady}
-                languages={languages} />
-            <GoogleTranslateDropdown
-                selectedLang={selectedLang}
-                setSelectedLang={setSelectedLang}
-                isReady={isComboReady}
-                languages={languages} />
-        </>
-    );
+const GoogleTranslate = ({}) => {
+  const [selectedLang, setSelectedLang] = useState(
+    localStorage.getItem("selectedLanguage") || "en"
+  );
+  const [isComboReady, setIsComboReady] = useState(false);
+  return (
+    <>
+      <GoogleTranslateLoader onReady={setIsComboReady} languages={languages} />
+      <GoogleTranslateDropdown
+        selectedLang={selectedLang}
+        setSelectedLang={setSelectedLang}
+        isReady={isComboReady}
+        languages={languages}
+      />
+    </>
+  );
 };
 
 export default GoogleTranslate;
