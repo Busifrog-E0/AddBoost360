3f1d43ac98d025d85683b44388fca86682e5d27a
Rajath.K.S <rajathkizhiyedam@gmail.com>
2025-08-10 10:24:56 +0530

    footer

--- apps/addboost-frontify/src/Components/VersionDisplay.jsx
+++
@@ -1,8 +0,0 @@
-// VersionDisplay.jsx
-function VersionDisplay() {
-    return <div>
-        <p className="text-xs text-gray-600 text-center font-inter"> Version: {__APP_VERSION__}</p>
-    </div>
-}
-
-export default VersionDisplay;
